7511219d430c807118a60bae1b0304c8f0f13c23
jpz2004 <147892167+jpz2004@users.noreply.github.com>
2024-12-11 17:45:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('ingresar').addEventListener('click', () => {
+    window.location.href = 'login.html';
+});
+
+document.getElementById('registro').addEventListener('click', () => {
+    window.location.href = 'registro.html';
+});
